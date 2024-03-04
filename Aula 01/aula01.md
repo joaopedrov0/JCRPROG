cfccec54d25de537c3be0b0db87eff8bc122ad77
João Pedro V <mastergamerjp06@gmail.com>
2024-03-04 12:45:22 +0000

    first commit

---
+++ Aula 01/aula01.md
@@ -0,0 +1,20 @@
+# Anotações da Aula 01 da disciplina JCRPROG
+
+## Entendendo a função "print()"
+
+A função print() é uma função responsável por imprimir valores na tela. Ela pode receber múltiplos argumentos separados por vírgula, e os exibe na tela com um separador definido na chamada, ou o separador padrão " " (espaço)
+
+```
+# Exemplo simples de uso da função "print()"
+print("Hello world")
+
+# Exemplo de uso com concatenação de múltiplas variáveis
+print("Hello", "world")
+
+# Alterando o separador padrão da função print() para múltiplos argumentos.
+print("Hello", "World", sep="+")
+
+# Quebra de linha
+print("Hello\nWorld")
+```
+
---
+++ Aula 01/aula01.py
@@ -0,0 +1,62 @@
+# Função print()
+
+print("Hello World")
+
+
+# Múltiplos argumentos na função print()
+
+print("Hello", "World")
+
+
+# Alterando o separador padrão da função print() para múltiplos argumentos.
+
+print("Hello", "World", sep="+")
+
+
+# Quebra de linha
+
+print("Hello\nWorld")
+
+
+# Alterando final do retorno da função print()
+
+print("Hello World", end="!")
+
+
+# Tipos Primitivos
+## Inteiro
+print(type(10))
+
+## Float
+print(type(10.0))
+
+## String
+print(type("Hello World"))
+
+## Booleano
+print(type(False))
+print(type(True))
+
+# Definição de variáveis
+
+pi = 3.1416 # definição da variável
+print(type(pi)) # Retorna float
+
+pi = 10 # sobrescrevendo o valor da variável
+print(type(pi)) # Retorna int
+
+
+# Múltiplas atribuições num comando
+a = b = c = 3
+print(a,b,c) # Retorna 3 3 3
+
+# Valores diferentes em múltiplas variáveis
+a,b,c = 1,2,3
+print(a,b,c) # Retorna 1 2 3
+
+
+# Python como linguagem case sensitive
+c1 = 5
+C1 = 10
+print(c1) # Imprime 5
+print(C1) # Imprime 10
